c35ad7b1662ba6e524281a0ffcc85fb5bbaab486
Chetan P Jain <chetan.jain@iamblacklotus.com>
2020-05-03 11:49:44 +0530

    Add json invalid to valid converter

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const fs = require('fs');
-const filePath = './daily_14.json'
+const fs = require("fs");
+const filePath = "./daily_14.json";
 
-fs.readFile(filePath, 'utf8', function(err, data) {
-    if(err) {
-        console.log('Error: ', err);
-        throw err
-    } else {
-        try {
-            const lines = data.split(/\r?\n/);
-            const cities = []
+fs.readFile(filePath, "utf8", function (err, data) {
+  if (err) {
+    console.log("Error: ", err);
+    throw err;
+  } else {
+    try {
+      const lines = data.split(/\r?\n/);
+      const cities = [];
 
-            lines.forEach((line) => {
-                if(line) {
-                    const cityObj = JSON.parse(line)
-                    if(cityObj.city.id && cityObj.city.name) {
-                        cities.push({ id: cityObj.city.id, name: cityObj.city.name })
-                    }
-                }
-            });
-
-            cities.sort((city1, city2) => {
-                return city1.name.toLowerCase().localeCompare(city2.name.toLowerCase())
-            });
-
-            fs.writeFile("./daily.json", JSON.stringify(cities), err => { 
-                if (err) {
-                    throw err;
-                } else {
-                    console.log("File created");
-                }
-            })
-        } catch(err) {
-            console.log('Error: ', err);
+      lines.forEach((line) => {
+        if (line) {
+          const cityObj = JSON.parse(line);
+          if (cityObj.city.id && cityObj.city.name) {
+            cities.push({ id: cityObj.city.id, name: cityObj.city.name });
+          }
         }
+      });
+
+      cities.sort((city1, city2) => {
+        return city1.name.toLowerCase().localeCompare(city2.name.toLowerCase());
+      });
+
+      fs.writeFile("./daily.json", JSON.stringify(cities), (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log("File created");
+        }
+      });
+    } catch (err) {
+      console.log("Error: ", err);
     }
-}); 
+  }
+});
